12dbff02ae4c06da37a1d086353981c367eb1bda
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-05-16 17:51:49 +0100

    google1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits,  Highlight } from "react-instantsearch-hooks-web";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+} from "react-instantsearch-hooks-web";
 import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Post from "./components/Post";
 import Project from "./components/Project";
 import SinglePost from "./components/SinglePost";
-
-
-
 
 const searchClient = algoliasearch(
   "PL5HEFXPXA",
@@ -31,20 +33,28 @@ function Hit({ hit }) {
   );
 }
 
-
-
 function App() {
-
   const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement({ pageLanguage: 'en', layout: window.google.translate.TranslateElement.FloatPosition.TOP_LEFT }, 'google_translate_element')
-   }
-   
-   useEffect(() => {
-     var addScript = document.createElement('script');
-     addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
-     document.body.appendChild(addScript);
-     window.googleTranslateElementInit = googleTranslateElementInit;
-   }, [])
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+      },
+      "google_translate_element"
+    );
+  };
+
+  //  layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
   return (
     <InstantSearch searchClient={searchClient} indexName="sanity_PORT">
       <NavBar />
@@ -56,8 +66,6 @@ function App() {
         <Route path="/post/:slug" element={<SinglePost />} />
         <Route path="/post" element={<Post />} />
         <Route path="/project" element={<Project />} />
-  
-     
       </Routes>
     </InstantSearch>
   );
